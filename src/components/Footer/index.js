149fd5f9f048b6d7c22b7c519a42a5892776d621
Loyanne Cristine <loyanne.cristine@gmail.com>
2020-07-31 22:38:14 +0100

    updated footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,7 +8,13 @@ function Footer() {
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
       </a>
       <p>
-        Proudly created with love and care during ♥
+        Proudly created with love and care during by
+        {' '}
+        <a href="https://www.linkedin.com/in/loyannec/">
+          Loyanne Cristine
+        </a>
+        {' '}
+        ♥
         {' '}
         <a href="https://www.alura.com.br/">
           Alura's React Immersion
